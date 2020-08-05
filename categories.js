4907490582786668c23a39a8d7cbb862d5cb863e
Billy Murray <billy.murray@oliveai.com>
2020-08-05 14:52:31 -0400

    Defined categories as constants

--- categories.js
+++ categories.js
@@ -1,9 +1,19 @@
+/** @constant {string} */
 const CONTROLLER = 'Controller';
+
+/** @constant {string} */
 const INTELLIGENCE = 'Intelligence';
+
+/** @constant {string} */
 const SENSOR = 'Sensor';
+
+/** @constant {string} */
 const SIDEKICK = 'Sidekick';
+
+/** @constant {string} */
 const UNKNOWN = 'Unknown';
 
+/** @constant {string[]} */
 const categories = [UNKNOWN, INTELLIGENCE, CONTROLLER, SENSOR, SIDEKICK];
 
 module.exports = Object.freeze({
