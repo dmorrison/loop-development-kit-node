cd0287e695a455559bfcf92253a5573ac6e5d995
Jon Bernard <jon.bernard@crosschx.com>
2020-08-14 12:42:32 -0400

    add OS enums

---
+++ src/operatingSystem.ts
@@ -0,0 +1,21 @@
+/** @module operatingSystem */
+
+/** @constant {string} */
+const UNKNOWN = 'unknown';
+
+/** @constant {string} */
+const WINDOWS = 'win32';
+
+/** @constant {string} */
+const MACOS = 'darwin';
+
+/** @constant {string} */
+const LINUX = 'linux';
+
+/** @constant {string} */
+const ANY = 'any';
+
+/** @constant {string[]} */
+const operatingSystems = [UNKNOWN, WINDOWS, MACOS, LINUX, ANY];
+
+export { UNKNOWN, WINDOWS, MACOS, LINUX, ANY, operatingSystems };
