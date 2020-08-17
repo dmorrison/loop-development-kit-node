5e6ec79ac874aa50ce5aa499d66fae8dbfeeb066
richseviora <richard.seviora@gmail.com>
2020-08-17 15:16:54 -0700

    [SIDE-477] Update docs

--- src/pluginHost.ts
+++ src/pluginHost.ts
@@ -1,7 +1,7 @@
 export interface PluginHost {
   /**
    * @param key - Key of the item to delete from storage.
-   * @return Promise that resolves when the deletion completes.
+   * @returns Promise that resolves when the deletion completes.
    */
   storageDelete(key: string): Promise<void>;
 
@@ -12,33 +12,36 @@ export interface PluginHost {
 
   /**
    * @param key - Key to check for presence.
-   * @return Promise resolve with whether the storage has the key (true) or not (false).
+   * @returns Promise resolve with whether the storage has the key (true) or not (false).
    */
   storageHasKey(key: string): Promise<boolean>;
 
   /**
-   * @return An array of all the keys currently present in storage.
+   * @returns An array of all the keys currently present in storage.
    */
   storageKeys(): Promise<string[]>;
 
   /**
    * Reads the value from storage and returns it in a promise.
+   *
    * @param key - Key to read from storage.
-   * @return Resolves with the value of the key.
+   * @returns Resolves with the value of the key.
    */
   storageRead(key: string): Promise<string>;
 
   /**
    * Reads all keys in storage.
-   * @return A promise resolving with an array of all the keys.
+   *
+   * @returns A promise resolving with an array of all the keys.
    */
   storageReadAll(): Promise<{ [index: string]: string }>;
 
   /**
    * Writes a value to storage.
+   *
    * @param key Key to write value to.
    * @param value The value being written.
-   * @return Resolves when write is complete.
+   * @returns Resolves when write is complete.
    */
   storageWrite(key: string, value: string): Promise<void>;
 }
--- src/sensorHost.ts
+++ src/sensorHost.ts
@@ -4,6 +4,7 @@ import { PluginHost } from './pluginHost';
 export interface SensorHost extends PluginHost {
   /**
    * Emits an event to Sidekick.
+   *
    * @param event - The event to emit.
    */
   emitEvent(event: PluginEvent): Promise<void>;
