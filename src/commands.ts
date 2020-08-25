ac0488f03a6cfaea9e005834b19bec7aa73e3db1
richseviora <richard.seviora@gmail.com>
2020-08-25 09:23:41 -0700

    [SIDE-476] Update documentation

--- src/commands.ts
+++ src/commands.ts
@@ -44,9 +44,14 @@ program
     '$npm_package_name',
   )
   .option('-b --buildCommand <command>', 'build command', 'build')
+  .option(
+    '--sensor',
+    'select if your plugin is a sensor (assumes controller by default)',
+  )
   .option('-d --dryRun', 'output commands to be executed')
   .action((options) => {
-    const deployCommand = `npm run build && mkdir -p "$HOME/Library/Application Support/SideKick/plugins/controllers/$npm_package_name" && cp build/* "$HOME/Library/Application Support/SideKick/plugins/controllers/$npm_package_name/"`;
+    const subFolder = options.sensor ? 'sensors' : 'controllers';
+    const deployCommand = `npm run build && mkdir -p "$HOME/Library/Application Support/SideKick/plugins/${subFolder}/$npm_package_name" && cp build/* "$HOME/Library/Application Support/SideKick/plugins/${subFolder}/$npm_package_name/"`;
     if (options.dryRun) {
       console.log(deployCommand);
       return;
