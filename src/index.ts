325280b76e0e5e695d350a89abbbed22c73d7924
richseviora <richard.seviora@gmail.com>
2020-09-23 11:28:49 -0700

    [RDE-427] Add consumer-visible interfaces to export

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,25 @@ import { Controller } from './controller';
 import { Logger } from './logging';
 import { Sensor } from './sensor';
 import { serveControllerPlugin, serveSensorPlugin } from './serve';
+import { SensorHost } from './sensorHost';
+import { PluginEvent } from './pluginEvent';
+import { ControllerHost } from './controllerHost';
+import { Whisper } from './whisper';
+import { WhisperStyle } from './whisperStyle';
 
 export {
   access,
   categories,
   operatingSystem,
   Controller,
+  ControllerHost,
   ControllerPlugin,
+  Whisper,
+  WhisperStyle,
   Sensor,
+  SensorHost,
   SensorPlugin,
+  PluginEvent,
   Logger,
   serveControllerPlugin,
   serveSensorPlugin,
